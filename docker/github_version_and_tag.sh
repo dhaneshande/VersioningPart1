41ea3234e292fe3d99d9d4e3ee81f81da6faf073
dhaneshande <dande@miraclesoft.com>
2017-07-19 22:37:40 +0530

    Update github_version_and_tag.sh

--- docker/github_version_and_tag.sh
+++ docker/github_version_and_tag.sh
@@ -3,11 +3,4 @@
 set -e # fail fast
 set -x # print commands
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-  
-echo $ PACKAGE_VERSION
+docker ps
